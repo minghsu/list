c9ae8da7d02b80e049f3e5857b04dedb0027e653
mingsheu <hsu.chihming@gmail.com>
2023-01-28 10:07:35 +0800

    [LIST] Initial Version

---
+++ list.h
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+#ifndef NULL
+#define NULL 0
+#endif
+
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+#define NULL_PTR_STR(p) (p) ? p : "NULL"
+
+#if INTPTR_MAX == INT64_MAX
+// 64-bit
+typedef u_int64_t LIST_HANDLE;
+typedef u_int64_t LIST_COUNT;
+typedef u_int64_t LIST_SIZE;
+typedef int64_t LIST_INDEX;
+#elif INTPTR_MAX == INT32_MAX
+typedef u_int32_t LIST_HANDLE;
+typedef u_int32_t LIST_COUNT;
+typedef u_int32_t LIST_SIZE;
+typedef int32_t LIST_INDEX;
+// 32-bit
+#else
+#error Unknown pointer size or missing size macros!
+#endif
+
+typedef enum
+{
+    LIST_ERR_NONE = 0,
+    LIST_ERR_INVALID_HANDLE = -1,
+    LIST_ERR_NO_MEMORY = -2,
+    LIST_ERR_EMPTY = -3,
+    LIST_ERR_INVALID_ARGUMENT = -4,
+    LIST_ERR_NOT_FOUND = -5
+} LIST_ERROR_E;
+
+/**
+ *  Memory ALLOC/FREE MACRO defined
+ **/
+#define LIST_MALLOC(p, s) p = list_malloc(s, __FILE__, __LINE__)
+#define LIST_FREE(p) list_free(p, __FILE__, __LINE__)
+
+/**
+ *  To enable/disable the debug info for memory ALLOC/FREE status
+ **/
+#define LIST_MEMORY_DEBUG 0
+
+/**
+ *  CRC32 Data Length, 0 for full data
+ *  <= : Calculate full data length
+ *  >  : Calculate LIST_CRC32_MAX_BYTE length
+ **/
+#define LIST_CRC32_MAX_BYTE 64
+
+typedef struct
+{
+    void *prev;
+    void *next;
+} LIST_INFO_T;
+
+typedef struct
+{
+    void *data;
+    LIST_SIZE size;
+    u_int32_t hash;
+} DATA_INFO_T;
+
+typedef struct
+{
+    LIST_INFO_T list;
+    DATA_INFO_T data;
+} LIST_ITEM_T;
+
+LIST_HANDLE list_initial(void);
+LIST_ERROR_E list_destory(LIST_HANDLE a_lst_hnd);
+LIST_ERROR_E list_push_head(LIST_HANDLE a_lst_hnd, void *ap_data, LIST_SIZE a_size);
+LIST_ERROR_E list_push_tail(LIST_HANDLE a_lst_hnd, void *ap_data, LIST_SIZE a_size);
+void *list_pop_head(LIST_HANDLE a_lst_hnd);
+void *list_pop_tail(LIST_HANDLE a_lst_hnd);
+LIST_INDEX list_search(LIST_HANDLE a_lst_hnd, void *ap_data, LIST_SIZE a_size);
+LIST_COUNT list_count(LIST_HANDLE a_lst_hnd);
+void *list_malloc(LIST_SIZE a_size, int8_t *ap_file, LIST_INDEX a_line);
+void list_free(void *ap_ptr, int8_t *ap_file, LIST_INDEX a_line);
+void list_dump(LIST_HANDLE a_lst_hnd);
---
+++ test.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "list.h"
+
+typedef struct
+{
+    void *data;
+    int size;
+} TEST_LIST_ITEM_T;
+
+#define MODEL_STACK 1
+#define MODEL_QUEUE 2
+
+void get_crc32_tbl(void)
+{
+    unsigned int crcTable[256], crcTmp1;
+
+    for (int i = 0; i < 256; i++)
+    {
+        crcTmp1 = i;
+        for (int j = 8; j > 0; j--)
+        {
+            if (crcTmp1 & 1)
+                crcTmp1 = (crcTmp1 >> 1) ^ 0xEDB88320L;
+            else
+                crcTmp1 >>= 1;
+        }
+        crcTable[i] = crcTmp1;
+
+        if (i % 8 == 0)
+        {
+            printf("\n0x%x, ", crcTmp1);
+        }
+        else
+        {
+            printf("0x%x, ", crcTmp1);
+        }
+    }
+    printf("\n");
+}
+
+int test_list(int model)
+{
+    unsigned int count = (rand() % 10) + 1;                // 1 ~ 1000
+    unsigned int test_search_index = (rand() % count) + 1; // 1 ~ count
+    int test_search_match_index = -1;
+    // unsigned int count = 10;
+    TEST_LIST_ITEM_T *test_item = NULL;
+    TEST_LIST_ITEM_T test_search_item;
+
+    printf("Count: %u, Test Index: %u\n", count, test_search_index);
+    u_int64_t list = list_initial();
+
+    // add test item
+
+    printf("Start Timestamp: %d\n", (int)time(NULL));
+    // for (unsigned int i = count; i < 0; i--)
+    for (unsigned int i = 0; i < count; i++)
+    {
+        LIST_MALLOC(test_item, sizeof(TEST_LIST_ITEM_T));
+        if (test_item)
+        {
+            test_item->size = (rand() % 1024) + 1; // 1 ~ 1024
+            LIST_MALLOC(test_item->data, test_item->size);
+
+            if (model == MODEL_QUEUE)
+                list_push_tail(list, test_item, sizeof(TEST_LIST_ITEM_T));
+            else
+                list_push_head(list, test_item, sizeof(TEST_LIST_ITEM_T));
+
+            // printf("ADD > Item: %p, Data: %p, Size: %d\n", test_item, test_item->data, test_item->size);
+
+            if ((i + 1) == test_search_index)
+            {
+                memset(&test_search_item, 0, sizeof(TEST_LIST_ITEM_T));
+                memcpy(&test_search_item, test_item, sizeof(TEST_LIST_ITEM_T));
+            }
+        }
+    }
+    printf("End Timestamp: %d\n", (int)time(NULL));
+
+    list_dump(list);
+
+    printf("search item data: %p, size: %d\n", test_search_item.data, test_search_item.size);
+    test_search_match_index = list_search(list, (void *)&test_search_item, sizeof(TEST_LIST_ITEM_T));
+    printf("search result: %d\n", test_search_match_index);
+
+    // free test item
+    test_item = list_pop_head(list);
+    while (test_item)
+    {
+        // printf("REMOVE > Item: %p, Data: %p, Size: %d\n", test_item, test_item->data, test_item->size);
+
+        LIST_FREE(test_item->data);
+        LIST_FREE(test_item);
+        test_item = list_pop_head(list);
+    }
+
+    list_destory(list);
+}
+
+int main(int argc, char *argv)
+{
+    srand(time(NULL));
+
+    // get_crc32_tbl();
+    printf("Test STACK model\n");
+    test_list(MODEL_STACK);
+
+    printf("\n\nTest QUEUE model\n");
+    test_list(MODEL_QUEUE);
+    return 0;
+}
